53b0dc396b5e0ee149e86d119415c5bb69bc5b3d
Connor Holloway <root_pfad@protonmail.com>
2020-09-13 18:50:20 +0100

    Digest trait

---
+++ src/traits.rs
@@ -0,0 +1,25 @@
+pub trait Digest {
+    /// Add an item to the digest
+    ///
+    /// # Arguments
+    ///
+    /// * `item` The item to add to the digest
+    fn add(&mut self, item: f64);
+
+    /// Add a buffer to the digest
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` The buffer to merge into the digest
+    fn add_buffer(&mut self, buffer: Vec<f64>);
+
+    /// Estimate the quantile of an item
+    /// # Arguments
+    /// * `value` Item to estimate the quantile of
+    fn est_quantile_at_value(&self, value: f64) -> f64;
+
+    /// Estimate the value at a particular quantile
+    /// # Arguments
+    /// * `quantile` The quantile to estimate the value of. 0 <= `quantile` <= 1.
+    fn est_value_at_quantile(&self, quantile: f64) -> f64;
+}
